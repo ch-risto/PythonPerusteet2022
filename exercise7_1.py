a18965f3b8116f8edc03b0e9ebe8320f9b006847
Christa <eloranta.christa@gmail.com>
2022-11-10 21:44:28 +0200

    Kokeillaan miten tiedostojen siirtäminen GitHubiin onnistuu part2

---
+++ exercise7_1.py
@@ -0,0 +1,26 @@
+cafe = {
+    "name": "Imaginary Cafe Oy",
+    "website":  "https://edu.frostbit.fi/sites/cafe",
+    "categories": [
+        "cafe",
+        "tea",
+        "lunch",
+        "breakfast"
+    ],
+    "location": {
+        "city": "Rovaniemi",
+        "address":  "Testikuja 22",
+        "zip_code": "96200"
+    }
+}
+
+# Haetaan dictionaryn sisällä olevista listoista tietoja apumuuttujiin
+services = ", ".join(cafe["categories"])
+address = cafe["location"]["zip_code"] + " " + cafe["location"]["city"]
+
+print(cafe["name"])
+print(cafe["location"]["address"])
+print(address)
+print()
+print(cafe["website"])
+print(f"Palvelut: {services}")
