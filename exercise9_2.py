3dbf4c2986b00c58dc2fee4778cd2ff0931a4958
Christa <eloranta.christa@gmail.com>
2022-12-08 11:20:23 +0200

    Kommentoitu palautettu versio

--- exercise9_2.py
+++ exercise9_2.py
@@ -1,7 +1,11 @@
 from functions import *
 
+# pyydetään käyttäjää syöttämään aika
 time = input("Syötä aika: (xxh xxmin xxsek)\n")
 
+# muutetaan syötetty aika tunneiksi minuuteiksi ja sekunneiksi funktiota varten
+# codepost vaati version, jossa funktioon syötettiin kolme arvoa, alunperin tämä
+# osa oli funktiossa
 time = time.split(" ")
 hours = time[0]
 hours = int(hours[:-1])
@@ -10,6 +14,8 @@ minutes = int(minutes[:-3])
 seconds = time[2]
 seconds = int(seconds[:-3])
 
+# kutsutaan funktio
 seconds = count_seconds(hours, minutes, seconds)
 
+# tulostetaan vastaus
 print(f"Yhteensä {seconds} sekuntia.")
