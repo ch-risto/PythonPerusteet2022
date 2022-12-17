4abc9b2604fae6958739782b5c0c58b48354c3fa
Christa <eloranta.christa@gmail.com>
2022-12-17 23:12:42 +0200

    Päivitetty niin, että käytetään funktiota (Harjotus9_7). Palautettu codepostiin

--- exercise3_7.py
+++ exercise3_7.py
@@ -1,40 +1,48 @@
 # Postihintalaskuri!
+def countMailingCost(item, weight, large_letter):
+    # luodaan apumuuttuja painon hintakertoimelle
+    weight_factor = 0
+    price = 0
 
-# Pyydetään käyttäjältä lähetyksen tyyppi ja paino
+    # Erilaiset hintavariaatiot kirjeelle
+    if item == "kirje":
+        price = 0.5
 
-item = input("Syötä lähetyksen tyyppi: paketti/kirje\n")
-weight = input("Syötä lähetyksen paino g:\n")
-weight = int(weight)
-price = 0
-fit_to_mail = True
+        if weight >= 500:
+            weight_factor = 0.07
 
-# erilaiset hintavariaatiot kirjeelle:
+            # Jos ei mahdu postilaatikkoon, lisätään hintaan kaksi euroa
+            if largeLetter:
+                price = price + 2
 
-if item == "kirje":
-    if weight < 200:
-        price = .50
+        if weight >= 200:
+            weight_factor = 0.04
 
-    elif 200 <= weight < 500:
-        price = round(.5 + weight//100 * .04, 2)
-
-    elif weight >= 500:
-        fit_to_mail = input("Mahtuuko kirje postilaatikkoon: K/E\n")
-        if fit_to_mail == "K":
-            price = round(.5 + weight//100 * .07, 2)
-
-        elif fit_to_mail == "E":
-            price = round(.5 + weight//100 * .07 + 2, 2)
-
-# erilaiset hintavariaatiot paketille:
-
-elif item == "paketti":
-    if weight < 200:
+    # erilaiset hintavariaatiot paketille:
+    elif item == "paketti":
         price = 2
 
-    elif 200 <= weight < 500:
-        price = round(2 + weight//100 * .08, 2)
+        if weight >= 500:
+            weight_factor = 0.14
 
-    elif weight >= 500:
-            price = round(2 + weight//100 * .14, 2)
+        elif weight >= 200:
+            weight_factor = 0.08
 
+    # lasketaan hinta: tyyppikohtainen perusmaksu plus tyypin ja painoluokan mukaan
+    # määräytyvä lisämaksu per 100g
+    price = price + weight // 100 * weight_factor
+
+    return round(price, 2)
+
+
+# Pyydetään käyttäjältä lähetyksen tyyppi ja paino
+item = input("Syötä lähetyksen tyyppi: paketti/kirje\n").lower()
+weight = int(input("Syötä lähetyksen paino g:\n"))
+large_letter = False
+if item == "kirje" and weight >= 500:
+    fit_to_mail = input("Mahtuuko kirje postilaatikkoon: K/E\n").upper()
+    if fit_to_mail == "E":
+        large_letter = True
+
+price = countMailingCost(item, weight, large_letter)
 print(f"Hinta on {price}€")
--- exercise5_8.py
+++ exercise5_8.py
@@ -1,16 +1,22 @@
+# luodaan funktio Caesarin salakirjoituksen käyttämiseksi
+# Samalla koodilla saa myös purettua "salakirjoituksen", jos tietää siirtoluvun
+# ja syöttää sen negatiivisena
+def caesarCipher(word, count):
+    # luodaan muuttuja, johon tallennetaan uusi sana
+    new_word = ""
+
+    # käydään sana läpi silmukassa
+    for n in word:
+        # kirjaimen sijainti merkistössä
+        index_n = ord(n)
+        # merkki annetulla sijoitusluvulla (kirjaimen sijainti + annettu siirtoluku)
+        char_n = chr(index_n + count)
+        new_word = new_word + char_n
+
+    return new_word
+
 # Pyydetään käyttäjältä sana ja siirtoluku
 word = input("Syötä sana:\n")
 count = int(input("Anna siirtoluku:\n"))
 
-# luodaan muuttuja, johon tallennetaan uusi sana
-new_word = ""
-
-# käydään sana läpi silmukassa
-for n in word:
-    # kirjaimen sijainti merkistössä
-    index_n = ord(n)
-    # merkki annetulla sijoitusluvulla (kirjaimen sijainti + annettu siirtoluku)
-    char_n = chr(index_n + count)
-    new_word = new_word + char_n
-
-print(new_word)
+print(caesarCipher(word, count))
